5af71437f88cafdb48957a3dcd368d84c6349304
andrey nikolaev <leopard.nikolaev@gmail.com>
2023-07-12 05:33:16 +0300

    commit

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 1, ya = 1, 
-    xb = 1, yb = 30,
-    xc = 30, yc = 30;
+int xa = 20, ya = 1, 
+    xb = 1, yb = 40,
+    xc = 40, yc = 40;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -15,31 +15,33 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
+
 int x = xa, y = xb;
 
-int count = 10;
+int count = 0;
 
-while(count <10)
+while(count < 10000)
 {
-    int what = new Random().Next(0, 3); // [0;3] 0 1 2
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
     if(what == 0)
     {
-        x= (x + xa) /2;
-        y = (y+ya)/2;
-
-    }
-    if(what == 1)
-    {
-        x = (x+xb) / 2;
-        y = (y + yc) / 2;
-    }
-    if(what == 1)
-    {
-        x = (x+xb) / 2;
-        y = (y + yc) / 2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
 
-    Console.SetCursorPosition(x,y);
+    if(what == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+
+    if(what == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count++;
+    count = count + 1;
 }
