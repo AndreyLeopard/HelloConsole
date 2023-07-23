9b4b4480e8cc2093444ff1e662c2643acd4985e8
andrey nikolaev <leopard.nikolaev@gmail.com>
2023-07-23 23:32:23 +0300

    add newText 'к', 'К'

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -127,4 +127,8 @@ string Replace(string text, char oldValue, char newValue)
     return result;
 }
 string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
