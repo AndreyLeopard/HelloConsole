ec4a4f0ae95254962b1b115631952e4b5f3a8611
andrey nikolaev <leopard.nikolaev@gmail.com>
2023-07-23 23:25:31 +0300

    add Replace string newText

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -96,3 +96,35 @@ for (int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+
+//========= Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточкой,
+// маленькие буквы "к" замеить боьшими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//              012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
