45eb9cad900b639f83966bd5f8719a03e2d262c4
andrey nikolaev <leopard.nikolaev@gmail.com>
2023-07-23 23:56:11 +0300

    add array.legth minPosition

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -85,7 +85,7 @@ string Method4(int count, string text)
     return result;
 }
 
-string res = Method4(10, "z");
+/* string res = Method4(10, "z");
 //Console.WriteLine(res);
 
 for (int i = 2; i <= 10; i++)
@@ -95,7 +95,7 @@ for (int i = 2; i <= 10; i++)
         Console.WriteLine($"{i} x {j} = {i * j}");
     }
     Console.WriteLine();
-}
+} */
 
 //========= Работа с текстом
 // Дан текст. В тексте нужно все пробелы заменить черточкой,
@@ -126,9 +126,54 @@ string Replace(string text, char oldValue, char newValue)
 
     return result;
 }
-string newText = Replace(text, ' ', '|');
+/* string newText = Replace(text, ' ', '|');
 
 Console.WriteLine(newText);
 Console.WriteLine();
 newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
+ */
+
+
+/* Функции в программировании
+
+1. Найти позицию минимального элемента в неотсортирванной части массива
+2. Произвести обмен этого значения со значением первой неотсортирвоанной позиции
+3. Повторять пока есть не отсортированные элементы
+ */
+
+ int[] arr ={ 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+ void PrintArray(int[] array)
+ {
+    int count = array.Length;
+
+    for (int i =0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+ }
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        //
+        for  (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[i] < array[minPosition]) minPosition = j;
+        }
+        //
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+ PrintArray(arr);
+ SelectionSort(arr);
+
+ PrintArray(arr);
