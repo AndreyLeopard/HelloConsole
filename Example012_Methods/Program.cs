d04f20682d8e2155533d50664690169795231c1e
andrey nikolaev <leopard.nikolaev@gmail.com>
2023-07-25 11:37:12 +0300

    add new console

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -142,7 +142,7 @@ Console.WriteLine(newText);
 3. Повторять пока есть не отсортированные элементы
  */
 
- int[] arr ={ 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+/*  int[] arr ={ 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
  void PrintArray(int[] array)
  {
@@ -171,6 +171,30 @@ void SelectionSort(int[] array)
         array[minPosition] = temporary;
     }
 }
+ */
+
+/*
+д/з каким образом упорядочевание было в обратном порядке, снчаал большой зачем меньше и меньше
+
+ Функции в программировании
+void SelectionSort(int[] array)
+{
+ for (int i = 0; i < array.Length - 1; i++)
+ {
+ int minPosition = i;
+ for (int j = i + 1; j < array.Length; j++)
+ {
+ if (array[j] < array[minPosition])
+ {
+ minPosition = j;
+ }
+ }
+ int temporary = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temporary;
+ }
+}
+*/ 
 
 
  PrintArray(arr);
